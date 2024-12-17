e9ea417d7fb83a10bfe34edbe7e66482acf33cfa
bluecco <bluecco@gmail.com>
2024-12-17 10:43:46 +0100

    chore: trigger deploy

--- src/components/StarknetDapp.tsx
+++ src/components/StarknetDapp.tsx
@@ -32,6 +32,7 @@ const StarknetDapp = () => {
               computational integrity.
             </span>
           </div>
+
           <div className="w-full hidden md:grid sm:gap-5 lg:gap-10 xl:gap-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             <AccountStatus />
           </div>
