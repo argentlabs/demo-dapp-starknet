e2661f5a9972cc2a1e3a2afc9fc3bbf036f63fd0
bluecco <bluecco@gmail.com>
2026-01-07 13:27:56 +0100

    fix: update starknetjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,41 @@
 
 import { StarknetDapp } from "@/components/StarknetDapp"
 import { connectors } from "@/connectors"
+import { IS_MAINNET } from "@/constants"
 import { mainnet, sepolia } from "@starknet-react/chains"
-import { publicProvider, StarknetConfig } from "@starknet-react/core"
+import {
+  jsonRpcProvider,
+  /* publicProvider, */
+  StarknetConfig,
+} from "@starknet-react/core"
 
 export default function Home() {
   const chains = [mainnet, sepolia]
-  const providers = publicProvider()
+  /* const providers = publicProvider() */
+
+  const mainnetJsonRpcProvider = jsonRpcProvider({
+    rpc: (chain) => {
+      console.log("mainnet", chain)
+      return {
+        nodeUrl: "https://rpc.starknet.lava.build",
+      }
+    },
+  })
+
+  const sepoliaJsonRpcProvider = jsonRpcProvider({
+    rpc: (chain) => {
+      console.log("sepolia", chain)
+      return {
+        nodeUrl: "https://rpc.starknet-sepolia.lava.build",
+      }
+    },
+  })
 
   return (
     <div className="flex flex-col flex-grow">
       <StarknetConfig
         chains={chains}
-        provider={providers}
+        provider={IS_MAINNET ? mainnetJsonRpcProvider : sepoliaJsonRpcProvider}
         connectors={connectors}
       >
         <StarknetDapp />
